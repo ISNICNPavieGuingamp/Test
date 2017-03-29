064422fc27e68faff2453ee6d7016f55337b8f2e
fabienvergniaud <fabien.vergniaud@gmail.com>
2017-03-29 18:20:30 +0200

    test
    Juste un test

---
+++ Balle1.py
@@ -0,0 +1,47 @@
+﻿# Créé par Fabien, le 30/04/2016 en Python 3.2
+import pygame,sys
+from pygame.locals import *
+
+
+
+pygame.init()
+
+
+size = width, height = 320, 240
+
+speed = [1, 1]
+
+black = 255, 255, 255
+
+
+screen = pygame.display.set_mode(size)
+
+
+ball = pygame.image.load("balle.png")
+
+ballrect = ball.get_rect()
+
+
+while 1:
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT: sys.exit()
+
+
+    ballrect = ballrect.move(speed)
+
+    if ballrect.left < 0 or ballrect.right > width:
+
+        speed[0] = -speed[0]
+
+    if ballrect.top < 0 or ballrect.bottom > height:
+
+        speed[1] = -speed[1]
+
+
+    screen.fill(black)
+
+    screen.blit(ball, ballrect)
+
+    pygame.display.flip()
